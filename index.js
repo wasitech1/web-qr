5ce7cdb86c56a50c8c7fced27b58502dee1bbc8e
wasitech1 <161101271+wasitech1@users.noreply.github.com>
2024-02-24 18:40:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ╔════◇
-║ *『 WAOW YOU CHOOSE SUHAIL-MD 』*
+║ *『 WAOW YOU CHOOSE WASI MD』*
 ║ _You complete first step to making Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║ *Ytube:* _youtube.com/SuhailTechInfo_
-║ *Owner:* _https://wa.me/923184474176_
+║ *Ytube:* _youtube.com/wasitech1_
+║ *Owner:* _https://wa.me/923192173398_
 ║ *Note :*_Don't provide your SESSION_ID to_
 ║ _anyone otherwise that can access chats_
 ╚════════════════════════╝
